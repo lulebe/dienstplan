d4f245afdc81a21df13e35e0d9ed6e366fc31b06
Leander Berg <reg@lulebe.net>
2020-10-29 14:17:55 +0100

    fixed plan creation

--- routes/app/main.js
+++ routes/app/main.js
@@ -2,7 +2,7 @@ const tmpl = require.main.require('./templates')
 const { Plan } = require.main.require('./db')
 
 module.exports = async (req, res) => {
-  const plans = await Plan.findAll()
+  const plans = await Plan.findAll({order: [['start', 'DESC']]})
   res.tmplOpts.plans = plans.map(p => p.dataValues)
   tmpl.render('app/main.twig', res.tmplOpts).then(rendered => res.end(rendered))
 }
--- routes/app/newplan_post.js
+++ routes/app/newplan_post.js
@@ -2,7 +2,7 @@ const { Plan } = require.main.require('./db')
 
 module.exports = async (req, res) => {
   console.log(req.body)
-  if (!req.body.name || !req.body.startdate || !req.body.starttime || !req.body.enddate || !req.body.endtime)
+  if (!req.body.name || !req.body.startdate || !req.body.enddate)
     return res.redirect('/app/newplan?status=0')
   await Plan.create({
     name: req.body.name,
