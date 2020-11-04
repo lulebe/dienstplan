0841c2a5370d590f33eae7ceb230e41760668236
Leander Berg <reg@lulebe.net>
2020-11-04 13:10:52 +0100

    admins can change more user data

--- routes/app/edituser_post.js
+++ routes/app/edituser_post.js
@@ -1,4 +1,3 @@
-const config = require.main.require('./config')
 const { User } = require.main.require('./db')
 
 module.exports = async (req, res, next) => {
@@ -7,14 +6,45 @@ module.exports = async (req, res, next) => {
   if (req.body.makeAdmin) {
     u.isAdmin = true
     await u.save()
+    res.tmplOpts.successMsg = "Änderungen gespeichert."
   }
   if (req.body.removeAdmin) {
     u.isAdmin = false
     await u.save()
+    res.tmplOpts.successMsg = "Änderungen gespeichert."
   }
+  if (req.body.mailChange) await mailChange(u, req, res, next)
+  if (req.body.nameChange) await nameChange(u, req, res, next)
+  if (req.body.phoneChange) await phoneChange(u, req, res, next)
   if (req.body.delete) {
     await u.destroy()
     return res.redirect('/app/admin')
   }
   next()
+}
+
+async function nameChange (u, req, res) {
+  if (req.body.firstName && req.body.lastName) {
+    u.firstName = req.body.firstName
+    u.lastName = req.body.lastName
+    await u.save()
+    res.tmplOpts.successMsg = "Änderungen gespeichert."
+  }
+}
+
+async function phoneChange (u, req, res) {
+  if (req.body.phone) {
+    u.phone = req.body.phone
+    await u.save()
+    res.tmplOpts.successMsg = "Änderungen gespeichert."
+  }
+}
+
+async function mailChange (u, req, res) {
+  if (req.body.newmail) {
+    const oldmail = u.email
+    u.email = req.body.newmail
+    await u.save()
+    res.tmplOpts.successMsg = "Änderungen gespeichert."
+  }
 }
